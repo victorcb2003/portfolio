3635e069c10eaac0f8c4cf62e7054b66ae31f15f
Victor Combemorel--Bluntz <victor.combemorel@gmai.com>
2026-05-03 21:19:37 +0200

    a

--- public/TreeJs/app.js
+++ public/TreeJs/app.js
@@ -1,4 +1,4 @@
-import * as THREE from "./node_modules/three/build/three.module.js";
+import * as THREE from "three";
 import { CameraControls } from "./modules/cameraControls.js";
 import { addStar } from "./modules/addStar.js";
 import { Planet } from "./modules/planet.js";
